a47e1d37d1b4487361ae892fd265dd91c8af2217
gkintana <81775867+gkintana@users.noreply.github.com>
2022-07-04 00:29:20 +0400

    player position & linux keycodes

--- include/cub3D.h
+++ include/cub3D.h
@@ -6,38 +6,59 @@
 /*   By: gkintana <gkintana@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 10:49:59 by gkintana          #+#    #+#             */
-/*   Updated: 2022/07/03 22:18:19 by gkintana         ###   ########.fr       */
+/*   Updated: 2022/07/03 23:36:24 by gkintana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
-#define CUB3D_H
+# define CUB3D_H
 
-#include <libft.h>
-#include <mlx.h>
-#include <fcntl.h>
-#include <math.h>
-#include <stdio.h>
+/*--------------------------------- LIBRARIES --------------------------------*/
+# include <libft.h>
+# include <mlx.h>
+# include <fcntl.h>
+# include <math.h>
+# include <stdio.h>
 
-typedef struct s_img
+/*--------------------------------- KEY CODES --------------------------------*/
+# if __linux__
+#  define KEYCODE_W		119
+#  define KEYCODE_A		97
+#  define KEYCODE_S		115
+#  define KEYCODE_D		100
+#  define KEYCODE_ESC	65307
+# endif
+
+// # if __APPLE__
+// #  define KEYCODE_W		119
+// #  define KEYCODE_A		97
+// #  define KEYCODE_S		115
+// #  define KEYCODE_D		100
+// #  define KEYCODE_ESC	65307
+// # endif
+
+/*-------------------------------- STRUCTURES --------------------------------*/
+typedef struct s_data
 {
 	void	*mlx;
 	void	*window;
+	int		width;
+	int		height;
 	char	**map;
 
-	void	*wall;
-	void	*player;
-	
+
 	char	*white;
 	char	*yellow;
-	
+
+	void	*wall;
 	int		wall_width;
 	int		wall_height;
+	
+	void	*player;
 	int		player_width;
 	int		player_height;
-	
 	int		x;
 	int		y;
-}				t_img;
+}				t_data;
 
 #endif
--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: gkintana <gkintana@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 01:01:52 by gkintana          #+#    #+#             */
-/*   Updated: 2022/07/03 22:31:55 by gkintana         ###   ########.fr       */
+/*   Updated: 2022/07/04 00:27:38 by gkintana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,11 +94,11 @@ void	free_2d_array(char **array)
 	array = NULL;
 }
 
-int	close_window(t_img *img)
+int	close_window(t_data *data)
 {
-	mlx_clear_window(img->mlx, img->window);
-	mlx_destroy_window(img->mlx, img->window);
-	free_2d_array(img->map);
+	free_2d_array(data->map);
+	mlx_clear_window(data->mlx, data->window);
+	mlx_destroy_window(data->mlx, data->window);
 	exit(0);
 }
 
@@ -106,87 +106,110 @@ int	close_window(t_img *img)
  * i[0] = index for 2D array
  * i[1] = index to read each element in array[i[0]]
  */
-void	plot_map(t_img *img)
+void	plot_map(t_data *data)
 {
 	int	i[2];
 
 	i[0] = -1;
-	while(img->map[++i[0]])
+	while(data->map[++i[0]])
 	{
 		i[1] = -1;
-		while (img->map[i[0]][++i[1]])
+		while (data->map[i[0]][++i[1]])
 		{
-			if (img->map[i[0]][i[1]] == '1')
+			if (data->map[i[0]][i[1]] == '1')
 			{
-				mlx_put_image_to_window(img->mlx, img->window, img->wall,
-				i[1] * img->wall_width, i[0] * img->wall_height);
+				mlx_put_image_to_window(data->mlx, data->window, data->wall,
+				i[1] * data->wall_width, i[0] * data->wall_height);
 			}
 		}
 	}
 }
 
-int	keyboard_events(int key_code, t_img *img)
+void	set_player_position(t_data *data)
 {
-	if (key_code == 65307)
-		close_window(img);
-	else if (key_code == 97)
+	int i[3];
+		
+	i[2] = 0;
+	i[0] = -1;
+	while (data->map[++i[0]])
 	{
-		if (img->map[img->y / img->wall_height][(img->x / img->wall_width) - 1] == '0')
-			img->x -= img->wall_width;
+		i[1] = -1;
+		while (data->map[i[0]][++i[1]])
+		{
+			if (data->map[i[0]][i[1]] == '0')
+			{
+				data->x = i[1] * data->player_width;
+				data->y = i[0] * data->player_height;
+				i[2] = 1;
+				break ;
+			}
+		}
+		if (i[2])
+			break ;
 	}
-	else if (key_code == 100)
+}
+
+int	keyboard_events(int input, t_data *data)
+{
+	if (input == KEYCODE_ESC)
+		close_window(data);
+	else if (input == KEYCODE_W)
 	{
-		if (img->map[img->y / img->wall_height][(img->x / img->wall_width) + 1] == '0')
-			img->x += img->wall_width;
+		if (data->map[(data->y / data->wall_height) - 1][data->x / data->wall_width] == '0')
+			data->y -= data->wall_height;
 	}
-	else if (key_code == 119)
+	else if (input == KEYCODE_A)
 	{
-		if (img->map[(img->y / img->wall_height) - 1][img->x / img->wall_width] == '0')
-			img->y -= img->wall_height;
+		if (data->map[data->y / data->wall_height][(data->x / data->wall_width) - 1] == '0')
+			data->x -= data->wall_width;
 	}
-	else if (key_code == 115)
+	else if (input == KEYCODE_S)
 	{
-		if (img->map[(img->y / img->wall_height) + 1][img->x / img->wall_width] == '0')
-			img->y += img->wall_height;
+		if (data->map[(data->y / data->wall_height) + 1][data->x / data->wall_width] == '0')
+			data->y += data->wall_height;
+	}
+	else if (input == KEYCODE_D)
+	{
+		if (data->map[data->y / data->wall_height][(data->x / data->wall_width) + 1] == '0')
+			data->x += data->wall_width;
 	}
 
-	// mlx_destroy_image(img->mlx, img->player);
-	mlx_clear_window(img->mlx, img->window);
-	plot_map(img);
-	mlx_put_image_to_window(img->mlx, img->window, img->player, img->x, img->y);
+	// mlx_destroy_image(data->mlx, data->player);
+	mlx_clear_window(data->mlx, data->window);
+	plot_map(data);
+	mlx_put_image_to_window(data->mlx, data->window, data->player, data->x, data->y);
 	return (0);
 }
 
 int	main(int argc, char **argv)
 {
-	t_img	img;
+	t_data	data;
 	int		map_lines;
 
 	if (argc == 2)
 	{
 		check_map_extension(argv[1]);
 		map_lines = check_map_validity(argv[1]);
-		img.map = save_map(argv[1], map_lines);
+		data.map = save_map(argv[1], map_lines);
 
-		img.mlx = mlx_init();
-		img.window = mlx_new_window(img.mlx, 1000, 800, "cub3D Test");
+		data.mlx = mlx_init();
+		data.width = 1000;
+		data.height = 800;
+		data.window = mlx_new_window(data.mlx, data.width, data.height, "cub3D");
 
-		img.white = "xpm/white.xpm";
-		img.wall = mlx_xpm_file_to_image(img.mlx, img.white, &img.wall_width, &img.wall_height);
-		img.yellow = "xpm/yellow.xpm";
-		img.player = mlx_xpm_file_to_image(img.mlx, img.yellow, &img.player_width, &img.player_height);
-		
-		plot_map(&img);
+		data.white = "xpm/white.xpm";
+		data.wall = mlx_xpm_file_to_image(data.mlx, data.white, &data.wall_width, &data.wall_height);
+		data.yellow = "xpm/yellow.xpm";
+		data.player = mlx_xpm_file_to_image(data.mlx, data.yellow, &data.player_width, &data.player_height);
 
-		img.x = 2 * img.player_width;
-		img.y = 2 * img.player_height;
-		mlx_put_image_to_window(img.mlx, img.window, img.player, img.x, img.y);
+		set_player_position(&data);
+		plot_map(&data);
+		mlx_put_image_to_window(data.mlx, data.window, data.player, data.x, data.y);
 
-
-		// mlx_do_key_autorepeaton(img.mlx);
-		mlx_key_hook(img.window, keyboard_events, &img);
-		mlx_hook(img.window, 17, 1L<<17, close_window, &img);
-		mlx_loop(img.mlx);
+		mlx_do_key_autorepeaton(data.mlx);
+		mlx_key_hook(data.window, keyboard_events, &data);
+		mlx_hook(data.window, 17, 1L<<17, close_window, &data);
+		mlx_loop(data.mlx);
 
 		return (0);
 	}
