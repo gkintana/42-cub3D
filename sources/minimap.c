aab2c969d54c9dd4456dbda6c847186b5879a9b0
gkintana <81775867+gkintana@users.noreply.github.com>
2022-08-07 14:02:22 +0400

    refactor space checking for minimap

--- sources/minimap.c
+++ sources/minimap.c
@@ -6,7 +6,7 @@
 /*   By: gkintana <gkintana@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 14:58:32 by gkintana          #+#    #+#             */
-/*   Updated: 2022/08/06 23:59:28 by gkintana         ###   ########.fr       */
+/*   Updated: 2022/08/07 14:00:33 by gkintana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,41 +94,73 @@ int	is_valid_coordinate(t_program *prog, int j[])
 	return (0);
 }
 
-int	prev_is_one(char *str, int j)
+int check_north_wall(t_program *prog, int j[])
 {
-	j--;
-	while (j > 0 && str[j])
+	int	x;
+	int	y;
+
+	x = j[1];
+	y = j[0];
+	while (--y > 0)
+	{
+		if (is_valid_coordinate(prog, j) && (prog->mlx.map[y][x] == '1'))
+			return (1);
+		else if (is_valid_coordinate(prog, j) && (prog->mlx.map[y][x] == '0'))
+			break ;
+	}
+	return (0);
+}
+
+int	check_south_wall(t_program *prog, int j[])
+{
+	int	x;
+	int	y;
+
+	x = j[1];
+	y = j[0];
+	while (prog->mlx.map[++y])
+	{
+		if (!ft_strchr(prog->mlx.map[y], '0') && y == get_map_height(prog) - 1)
+			return (1);
+		if (is_valid_coordinate(prog, j) && (prog->mlx.map[y][x] == '1'))
+			return (1);
+		else if (is_valid_coordinate(prog, j) && (prog->mlx.map[y][x] == '0'))
+			break ;
+	}
+	return (0);
+}
+
+int check_east_wall(char *str, int j)
+{
+	while (str[++j])
 	{
 		if (str[j] == '1')
 			return (1);
-		else if (str[j] == ' ')
-			j--;
-		else
+		else if (str[j] == '0')
 			break;
 	}
 	return (0);
 }
 
-int after_is_one(char *str, int j)
+int	check_west_wall(char *str, int j)
 {
-	j++;
-	while (str[j])
+	while (--j > 0)
 	{
 		if (str[j] == '1')
 			return (1);
-		else if (str[j] == ' ')
-			j++;
-		else
+		else if (str[j] == '0')
 			break;
 	}
 	return (0);
 }
 
-int check_above(t_program *prog, int y, int x)
+int	space_is_inside(t_program *prog, int j[])
 {
-	if (y >= 0 && y < get_map_height(prog)
-	&& x >= 0 && x < (int)ft_strlen(prog->mlx.map[y])
-	&& (prog->mlx.map[y][x] == '1' || prog->mlx.map[y][x] == ' '))
+	if (j[1] > find_wall_start(prog->mlx.map[j[0]])
+		&& check_north_wall(prog, j)
+		&& check_south_wall(prog, j)
+		&& check_east_wall(prog->mlx.map[j[0]], j[1])
+		&& check_west_wall(prog->mlx.map[j[0]], j[1]))
 		return (1);
 	return (0);
 }
@@ -163,11 +195,7 @@ static int	check_map_coordinates(t_program *prog, int i[], bool is_wall,
 		&& j[1] < find_wall_start(prog->mlx.map[j[0]]))
 			return (1);
 		else if (is_valid_coordinate(prog, j) && prog->mlx.map[j[0]][j[1]] == ' '
-		&& j[1] > find_wall_start(prog->mlx.map[j[0]])
-		&& prev_is_one(prog->mlx.map[j[0]], j[1])
-		&& after_is_one(prog->mlx.map[j[0]], j[1])
-		&& check_above(prog, j[0] - 1, j[1]))
-		// || check_below(prog->mlx.map[j[0]], j[1])))
+		&& space_is_inside(prog, j))
 			return (1);
 	}
 	else
